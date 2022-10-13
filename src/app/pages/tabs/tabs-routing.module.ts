4e6f252061192923c0e5e9da4ce7f711993bceea
alberto <albertosdh00@gmail.com>
2022-10-13 12:55:55 +0200

    Subo commit para arreglar las tabs

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { TabsPage } from './tabs.page';
+
 
 const routes: Routes = [
   {
-    path: '',
-    component: TabsPage
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'DAM',
+        children: [
+          {
+            path: '',
+            loadChildren: '../DAM/DAM.module#DAMModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/app/tabs/DAM',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
